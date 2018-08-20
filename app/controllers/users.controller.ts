03150b8f5fd9889921fb979a7f4bdd8b7ce2b8ea
jeremy woo <jeremy.woo@akqa.com>
2018-08-20 16:40:50 +1200

    examples with emulator.

--- app/controllers/users.controller.ts
+++ app/controllers/users.controller.ts
@@ -2,6 +2,8 @@
 import { Router, Request, Response } from 'express';
 import { UserDao } from '../models/UsersDao'; 
 import { CosmosClient } from "@azure/cosmos";
+var azure = require('azure-storage');
+
 
 var multer = require('multer')
 var MulterAzureStorage = require('multer-azure-storage')
@@ -14,20 +16,37 @@ const userDao = new UserDao(client, 'meetroomdb', 'Users');
 // Assign router to the express.Router() instance
 const router: Router = Router();
 
+// set AZURE_STORAGE_CONNECTION_STRING=DefaultEndpointsProtocol=http;AccountName='devstoreaccount1';AccountKey='Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=='
+//set HTTP_PROXY= http://127.0.0.1:8888
+
+
 router.get('/', (req: Request, res: Response) => {
     // Reply with a hello world when no name param is provided
-
-   userDao.init();
-   let a = userDao.getUser('1');
-
-   var upload = multer({
-    storage: new MulterAzureStorage({
-      azureStorageConnectionString: 'DefaultEndpointsProtocol=https;AccountName=mystorageaccount;AccountKey=mykey;EndpointSuffix=core.windows.net',
-      containerName: 'photos',
-      containerSecurity: 'blob'
-    })
-  });
-
+    
+    //userDao.init();
+    //let a = userDao.getUser('1');
+        
+    var blobService = azure.createBlobService("UseDevelopmentStorage=true");
+    
+    blobService.createContainerIfNotExists('taskcontainer', {
+        publicAccessLevel: 'blob'
+    }, function(error:any, result:any, response:any) {
+        if (!error) {
+            // if result = true, container was created.
+            // if result = false, container already existed.
+        }
+    });
+        
+    var upload = multer({
+        storage: new MulterAzureStorage({
+            azureStorageConnectionString: 'DefaultEndpointsProtocol=https;AccountName=mystorageaccount;AccountKey=mykey;EndpointSuffix=core.windows.net',
+            containerName: 'photos',
+            containerSecurity: 'blob'
+        })
+    });
+    
+    
+    
     res.send('login, World!');
 });
 
@@ -40,7 +59,7 @@ router.put('/', (req: Request, res: Response) => {
 router.delete('/', (req: Request, res: Response) => {
     // Extract the name from the request parameters
     let { name } = req.params;
-
+    
     // Greet the given name
     res.send(`delete, ${name}`);
 });
@@ -49,7 +68,7 @@ router.delete('/', (req: Request, res: Response) => {
 router.post('/create', (req: Request, res: Response) => {
     // Extract the name from the request parameters
     //let { name } = req.params;
-
+    
     console.log(req.body);
     // Greet the given name
     res.send(`create`);
@@ -59,7 +78,7 @@ router.post('/create', (req: Request, res: Response) => {
 router.post('/login', (req: Request, res: Response) => {
     // Extract the name from the request parameters
     let { name } = req.params;
-
+    
     // Greet the given name   
     res.send(`login, ${name}`);
 });
